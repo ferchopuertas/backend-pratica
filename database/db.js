445393e12fa1eaec77400c66a29beee0c6d0388b
ferchopuertas <138713637+ferchopuertas@users.noreply.github.com>
2024-09-16 01:47:20 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,13 @@
 const mysql = require('mysql2')
 require('dotenv').config();
+const { parse } = require('url');
+
+// Si tienes la URL completa en el archivo .env
+const dbUrl = process.env.MYSQL_URL || '';
+
+const { hostname: host, pathname, auth } = parse(dbUrl);
+const [user, password] = auth.split(':');
+const database = pathname.replace('/', ''); // Elimina la barra inicial del pathname
 
 const db = mysql.createPool({
     connectionLimit: 100,
