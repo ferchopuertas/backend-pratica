b2bd617ef3f72b48a80dc1b5704ca57f61007edd
ferchopuertas <138713637+ferchopuertas@users.noreply.github.com>
2024-09-16 02:35:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routerAsisNec = require('./routes/routeAsisNec');
 const routerLogin = require('./routes/routeLogin');
 const authenticateToken = require('./middleware/auth'); // Importa el middleware
 const app = express();
-const MYSQLPORT = process.env.MYSQLPORT || 3000
+const MYSQLPORT = process.env.MYSQLPORT || 3306
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
